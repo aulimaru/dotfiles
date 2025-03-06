eee6b84cf4c4df1710ef453f59b4837bc025621d
aulimaru <72508051+aulimaru@users.noreply.github.com>
2025-03-06 15:25:32 +0800

    Enable starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
     pyenv init - fish | source
-    # starship init fish | source
+    starship init fish | source
     abbr rm trash
     abbr nvim ec
     abbr cd z
